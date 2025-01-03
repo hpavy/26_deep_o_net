a1d5d05ed823c6b125bd25d78c0fd798a284667a
Hugo Pavy <hugo.pavy@gmail.com>
2025-01-03 12:24:18 +0100

    piche

---
+++ main.py
@@ -0,0 +1,61 @@
+import torch
+from run import RunSimulation
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Le code se lance sur {device}")
+
+
+folder_result_name = "9_without_dynamic"  # name of the result folder
+
+
+# On utilise hyper_param_init uniquement si c'est un nouveau modèle
+
+hyper_param_init = {
+    "H": 230.67,  # la rigidité du ressort
+    "ya0": [
+        0.0025,
+        0.0125,
+        0.00375,
+        0.00625,
+        0.00875,
+    ],  # la position initiale du ressort
+    "m": 1.57,  # la masse du ressort
+    "file": [
+        "data_john_2_case_2.csv",
+        "data_john_4_case_2.csv",
+        'data_john_5_case_2.csv',
+        'data_john_7_case_2.csv',
+        "data_john_9_case_2.csv",
+    ],
+    "nb_epoch": 1000,  # epoch number
+    "save_rate": 20,  # rate to save
+    "dynamic_weights": False,
+    "lr_weights": 1e-1,  # si dynamic weights
+    "weight_data": 0.33,
+    "weight_pde": 0.33,
+    "weight_border": 0.33,
+    "batch_size": 10000,  # for the pde
+    "nb_points_pde": 1000000,  # Total number of pde points
+    "Re": 100,
+    "lr_init": 0.001,
+    "gamma_scheduler": 0.999,  # pour la lr
+    "nb_layers": 12,
+    "nb_neurons": 64,
+    "n_pde_test": 5000,
+    "n_data_test": 5000,
+    "nb_points": 12*12,  # le nombre de points pris par axe par pas de temps
+    "x_min": -0.1,
+    "x_max": 0.1,
+    "y_min": -0.06,
+    "y_max": 0.06,
+    "t_min": 6.5,
+    "t_max": 8,
+    "nb_points_close_cylinder": 50,  # le nombre de points proches du cylindre
+    "nb_points_border": 25,  # le nombrede points sur la condition init
+}
+
+param_adim = {"V": 1.0, "L": 0.025, "rho": 1.2}
+
+simu = RunSimulation(hyper_param_init, folder_result_name, param_adim)
+
+simu.run()
---
+++ model.py
@@ -0,0 +1,179 @@
+import torch
+import torch.nn as nn
+from torch.autograd import grad
+
+
+def pde(
+    U,
+    input,
+    Re,
+    x_std,
+    y_std,
+    u_mean,
+    v_mean,
+    p_std,
+    t_std,
+    t_mean,
+    u_std,
+    v_std,
+    w_0,
+    L_adim,
+    V_adim,
+    ya0_mean,
+    ya0_std,
+):
+    # je sais qu'il fonctionne bien ! Il a été vérifié
+    """Calcul la pde
+
+    Args:
+        U (_type_): u,v,p calcullés par le NN
+        input (_type_): l'input (x,y,t)
+    """
+    u = U[:, 0].reshape(-1, 1)
+    v = U[:, 1].reshape(-1, 1)
+    p = U[:, 2].reshape(-1, 1)
+
+    u_X = grad(
+        outputs=u,
+        inputs=input,
+        grad_outputs=torch.ones_like(u),
+        retain_graph=True,
+        create_graph=True,
+    )[0]
+    v_X = grad(
+        outputs=v,
+        inputs=input,
+        grad_outputs=torch.ones_like(v),
+        retain_graph=True,
+        create_graph=True,
+    )[0]
+    p_X = grad(
+        outputs=p,
+        inputs=input,
+        grad_outputs=torch.ones_like(p),
+        retain_graph=True,
+        create_graph=True,
+    )[0]
+    u_x = u_X[:, 0].reshape(-1, 1)
+    u_y = u_X[:, 1].reshape(-1, 1)
+    u_t = u_X[:, 2].reshape(-1, 1)
+    v_x = v_X[:, 0].reshape(-1, 1)
+    v_y = v_X[:, 1].reshape(-1, 1)
+    v_t = v_X[:, 2].reshape(-1, 1)
+    p_x = p_X[:, 0].reshape(-1, 1)
+    p_y = p_X[:, 1].reshape(-1, 1)
+
+    # Dans les prochaines lignes on peut améliorer le code (on fait des calculs inutiles)
+    u_xx = grad(
+        outputs=u_x, inputs=input, grad_outputs=torch.ones_like(u_x), retain_graph=True
+    )[0][:, 0].reshape(-1, 1)
+    u_yy = grad(
+        outputs=u_y, inputs=input, grad_outputs=torch.ones_like(u_y), retain_graph=True
+    )[0][:, 1].reshape(-1, 1)
+    v_xx = grad(
+        outputs=v_x, inputs=input, grad_outputs=torch.ones_like(v_x), retain_graph=True
+    )[0][:, 0].reshape(-1, 1)
+    v_yy = grad(
+        outputs=v_y, inputs=input, grad_outputs=torch.ones_like(v_y), retain_graph=True
+    )[0][:, 1].reshape(-1, 1)
+
+    equ_1 = (
+        (u_std / t_std) * u_t
+        + (u * u_std + u_mean) * (u_std / x_std) * u_x
+        + (v * v_std + v_mean) * (u_std / y_std) * u_y
+        + (p_std / x_std) * p_x
+        - (1 / Re) * ((u_std / (x_std**2)) * u_xx + (u_std / (y_std**2)) * u_yy)
+    )
+    equ_2 = (
+        (v_std / t_std) * v_t
+        + (u * u_std + u_mean) * (v_std / x_std) * v_x
+        + (v * v_std + v_mean) * (v_std / y_std) * v_y
+        + (p_std / y_std) * p_y
+        - (1 / Re) * ((v_std / (x_std**2)) * v_xx + (v_std / (y_std**2)) * v_yy)
+        - (input[:, 3] * ya0_std + ya0_mean)
+        * L_adim
+        * w_0**2
+        * L_adim
+        * torch.cos((w_0 * t_std * input[:, 2]) / (t_mean))
+        / V_adim**2
+    )
+    equ_3 = (u_std / x_std) * u_x + (v_std / y_std) * v_y
+    return equ_1, equ_2, equ_3
+
+
+# Le NN
+
+
+class MLP(nn.Module):
+    def __init__(self, nb_entry, nb_neurons, nb_layers, nb_branches):
+        super().__init__()
+        self.init_layer = nn.ModuleList([nn.Linear(nb_entry, nb_neurons)])
+        self.hiden_layers = nn.ModuleList(
+            [nn.Linear(nb_neurons, nb_neurons) for _ in range(nb_layers - 1)]
+        )
+        self.final_layer = nn.ModuleList([nn.Linear(nb_neurons, nb_branches)])
+        self.layers = self.init_layer + self.hiden_layers + self.final_layer
+        self.initial_param()
+
+    def forward(self, x):
+        for k, layer in enumerate(self.layers):
+            if k != len(self.layers) - 1:
+                x = torch.tanh(layer(x))
+            else:
+                x = layer(x)
+        return x  # Retourner la sortie
+
+    def initial_param(self):
+        for layer in self.layers:
+            nn.init.xavier_uniform_(layer.weight)
+            nn.init.zeros_(layer.bias)
+
+
+class DeepONet(nn.Module):
+    def __init__(
+        self,
+        nb_entry_branch,
+        nb_entry_trunk,
+        trunk_width,
+        trunk_depth,
+        branch_width,
+        branch_depth,
+        nb_branches,
+        nb_exit,
+    ):
+        super().__init__()
+        self.trunk = MLP(
+            nb_entry_trunk, trunk_width, trunk_depth, nb_branches * nb_exit
+        )
+        self.branch = MLP(
+            nb_entry_branch, branch_width, branch_depth, nb_branches * nb_exit
+        )
+        self.trunk.initial_param()
+        self.branch.initial_param()
+        self.nb_branches = nb_branches
+        self.final_bias = nn.Parameter(torch.zeros(nb_exit))
+        self.nb_exit = nb_exit
+
+    def forward(self, x_trunk, x_branch):
+        product_branch = self.trunk(x_trunk) * self.branch(x_branch)
+        return (
+            torch.sum(
+                product_branch.reshape(self.nb_branches, -1, self.nb_exit),
+                dim=0,
+            )
+            + self.final_bias
+        )
+
+
+if __name__ == "__main__":
+    piche = DeepONet(
+        nb_entry_branch=1,
+        nb_entry_trunk=3,
+        trunk_width=64,
+        trunk_depth=2,
+        branch_width=64,
+        branch_depth=2,
+        nb_branches=20,
+        nb_exit=3,
+    )
+    print(piche(torch.randn(25, 3), torch.randn(25, 1)))
