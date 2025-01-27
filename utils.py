b76071c668e780bbc2943443d0c7bd25d3025791
hugopavy <hugo.pavy@etu.minesparis.psl.eu>
2025-01-27 09:07:22 +0000

    piche

--- utils.py
+++ utils.py
@@ -69,8 +69,7 @@ def charge_data(hyper_param, param_adim):
             torch.tensor(df_modified["Points:1"].to_numpy(), dtype=torch.float32)
             / param_adim["L"]
         )
-        time_without_modulo = df_modified["Time"].to_numpy() - hyper_param['t_min']
-        time_with_modulo = hyper_param['t_min'] + time_without_modulo % (1/f)
+
         t_full.append(
             torch.tensor(time_with_modulo, dtype=torch.float32)
             / (param_adim["L"] / param_adim["V"])
